83531aae716ebb2366d0bf84952a0025d270fb1e
Juho Rutila <juho.rutila@iki.fi>
2016-10-07 07:06:39 +0300

    Copy db fixes

--- tenant/management/commands/copy_db.py
+++ tenant/management/commands/copy_db.py
@@ -23,11 +23,26 @@ def dictfetchall(cursor):
         for row in cursor.fetchall()
     ]
 
-
-
-
 class Command(BaseCommand):
     help = 'Copy old stables database to the new one'
+    def customerfororder(self, old, new, **kwargs):
+        sql = """
+            select user_id from %(schema)s.stables_shop_ticketproductactivator tpa
+                INNER JOIN %(schema)s.stables_shop_productactivator pa ON pa.id = tpa.productactivator_ptr_id
+                INNER JOIN %(schema)s.stables_userprofile up ON up.rider_id = tpa.rider_id
+                WHERE order_id = %(id)d;
+            """
+        old.execute(sql % kwargs)
+        user_id = old.fetchone()
+        if user_id == None:
+            self.stdout.write("No dice with order id %d" % kwargs["id"])
+            return 0
+        return user_id[0]
+
+    def insertorderextra(self, new, data, schema):
+        for d in data:
+            d['schema'] = schema
+            new.execute("update %(schema)s.stables_shop_order set extra ='%(text)s' where id = %(order_id)d" % d)
 
     def add_arguments(self, parser):
          # Named (optional) arguments
@@ -60,7 +75,7 @@ class Command(BaseCommand):
         tables['stables_tickettype'] = {}
         tables['auth_user'] = {}
         tables['stables_shop_customer'] = {
-            "new": "insert into %s.stables_shop_customer select id, 0, '', date '1900-01-01', '{}' from %s.auth_user;"
+            "new": "insert into %(schema)s.stables_shop_customer select id, 0, '', date '1900-01-01', '{}' from %(schema)s.auth_user;"
         }
         tables['schedule_calendar'] = {}
         tables['schedule_rule'] = {}
@@ -68,14 +83,21 @@ class Command(BaseCommand):
         tables['schedule_occurrence'] = {}
         tables['shop_order'] = {
             "table": "stables_shop_order",
+            "where": "status = 50",
             "rename": {
                 "order_subtotal": "_subtotal", "order_total": "_total",
-                "created": "created_at", "modified": "updated_at"
-            }
+                "created": "created_at", "modified": "updated_at",
+            },
+            "script": {
+                "customer_id": self.customerfororder
+            },
+            "missing": ["cart_pk", "user_id"],
+            "defaults": { "currency": "EUR", "extra": "", "stored_request": "" }
         }
         tables['stables_shop_product'] = {
             "rename": { "name": "product_name", "date_added": "created_at", "last_modified": "updated_at" },
             "convert": { "polymorphic_ctype_id": "content_type" }}
+        """
         tables['shop_cart'] = {
             "table": "stables_shop_cart",
             "rename": { "date_created": "created_at", "last_updated": "updated_at", "user_id": "customer_id" },
@@ -87,13 +109,21 @@ class Command(BaseCommand):
         tables['shop_cartitem'] = {
             "table": "stables_shop_cartitem"
         }
+        """
         tables['shop_orderitem'] = {
-            "table": "stables_shop_orderitem"
+            "table": "stables_shop_orderitem",
+            "rename": { "line_total": "_line_total", "unit_price": "_unit_price", "product_reference": "product_code",
+                        },
+            "where": "order_id in (select id from %(schema_name)s.shop_order where status = 50)",
+            "missing": ["line_subtotal"],
+            "defaults": {"extra": ""}
         }
-        tables['shop_extraorderpricefield'] = { }
-        tables['shop_extraorderitempricefield'] = {}
-        tables['shop_orderextrainfo'] = {}
-        tables['shop_orderpayment'] = {}
+        #tables['shop_extraorderpricefield'] = { }
+        #tables['shop_extraorderitempricefield'] = {}
+        tables['shop_orderextrainfo'] = {
+            "data": self.insertorderextra
+        }
+        #tables['shop_orderpayment'] = {}
         tables['stables_course'] = {}
         tables['stables_horse'] = {}
         tables['stables_instructorinfo'] = {}
@@ -115,8 +145,8 @@ class Command(BaseCommand):
         tables['stables_participationtransactionactivator_ticket_type'] = {}
         tables['stables_riderinfo_levels'] = {}
         tables['stables_riderlevel_includes'] = {}
-        tables['stables_shop_address'] = {}
-        tables['stables_shop_digitalshippingaddressmodel'] = {}
+        #tables['stables_shop_address'] = {}
+        #tables['stables_shop_digitalshippingaddressmodel'] = {}
         tables['stables_shop_enrollproduct'] = {}
         tables['stables_shop_enrollproductactivator'] = {}
         tables['stables_shop_partshorturl'] = {}
@@ -171,17 +201,23 @@ class Command(BaseCommand):
                     self.stdout.write("Skipped "+tname)
                     continue
                 toname = tenant["schema_name"]+"."+t["table"] if "table" in t else tname
-                c.execute("SELECT * FROM %s" % tname)
-                data = dictfetchall(c)
                 opts = t
+
+                if not "new" in opts:
+                    qq = "SELECT * FROM %s" % tname
+                    if "where" in opts:
+                        qq = qq + " WHERE %s" % (opts["where"] % tenant)
+                    self.stdout.write("Fetch data with %s" % qq)
+                    c.execute(qq)
+                    data = dictfetchall(c)
                 fd = ""
                 conv = opts["convert"] if "convert" in opts else None
                 pconv = ttopts["preconvert"] if ttopts and "preconvert" in ttopts else None
                 counter = 0
                 ctcache = {}
                 if "new" in opts:
-                    w.execute(opts["new"] % tenant['schema_name'])
-                elif (len(data)):
+                    w.execute(opts["new"] % {"schema": tenant['schema_name']})
+                elif (len(data)) and "data" not in opts:
                     self.stdout.write("Writing %s to %s" % (tname, toname))
                     colnames = [x for x in data[0].keys() if "missing" not in opts or x not in opts["missing"]]
                     for d in data:
@@ -210,16 +246,21 @@ class Command(BaseCommand):
                         if "defaults" in opts:
                             for (dk,df) in opts["defaults"].items():
                                 vals.append(df)
-                        #vals = [conv[x](d[x]) if conv and x in conv else default_conv(d[x]) for x in colnames]
+                        if "script" in opts:
+                            for (sk,sf) in opts["script"].items():
+                                vals.append(sf(c,w,schema=tenant["schema_name"], **d))
+
                         fd = unicode(fd) + "\t".join([unicode(x) for x in vals]) + "\n"
-                        #(sql, vals) = insert(d, toname, t)
-                        #w.execute(sql, vals)
                     if "defaults" in opts:
                         for (dk,df) in opts["defaults"].items():
                             colnames.append(dk)
+                    if "script" in opts:
+                        for (sk,sf) in opts["script"].items():
+                            colnames.append(sk)
+
                     newcolnames = [x if "rename" not in opts or x not in opts["rename"] else opts["rename"][x] for x in colnames]
                     self.stdout.write(",".join(newcolnames) + " -> "+toname)
-                    self.stdout.write(fd)
+                    #self.stdout.write(fd)
                     w.copy_from(StringIO(unicode(fd)), toname, columns=['"'+x+'"' for x in newcolnames])
                     params = {"tbl": toname, "tblsc": toname.replace(tenant["schema_name"]+".", "")}
                     check = "SELECT relname FROM pg_class WHERE relname='%(tblsc)s_id_seq';" % params
@@ -228,6 +269,8 @@ class Command(BaseCommand):
                         id_seq = "SELECT setval('%(tbl)s_id_seq', COALESCE((SELECT MAX(id)+1 FROM %(tbl)s), 1), false)" % params
                         self.stdout.write(id_seq)
                         w.execute(id_seq)
+                elif (len(data)) and "data" in opts:
+                    opts["data"](w, data, tenant["schema_name"])
                 else:
                     self.stdout.write("No data for %s" % toname)
 
